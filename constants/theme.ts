2a88d35e48e234c6a90a3ecf1b41fdd8a90b3c39
alberme <albertm@alphaworks.tech>
2025-10-16 00:03:51 -0700

    feat(colors): add colors

--- constants/theme.ts
+++ constants/theme.ts
@@ -14,6 +14,8 @@ export const Colors = {
   icon: "#9BA1A6",
   tabIconDefault: "#9BA1A6",
   tabIconSelected: tintColorDark,
+  pink: "#FFCEEE",
+  blackText: "#08060A",
 };
 
 export const Fonts = Platform.select({
