fcc2ed32bc1096cc17d306c32ddfd286bc36013f
alberme <albertm@alphaworks.tech>
2025-10-15 23:56:49 -0700

    feat(route): add next screen routing

--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,36 @@
+import ContainerView from "@/components/ContainerView";
+import ScreenView from "@/components/ScreenView";
 import ThemedButton from "@/components/ThemedButton";
-import ThemedText from "@/components/ThemedText";
-import ThemedView from "@/components/ThemedView";
-import { StyleSheet} from "react-native";
-import {Image} from 'expo-image';
+import { Image } from "expo-image";
 import { useRouter } from "expo-router";
+import { StyleSheet, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
   const blurhash =
-  '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
-
+    "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
 
   return (
-
-      <ThemedView style={{ flex: 1, backgroundColor: "black", alignItems: "center", flexDirection: "column", }}>
-        <Image
-          source={require('@/assets/images/logo.png')}
-          placeholder={{blurhash}}
-          contentFit="scale-down"
-          style={styles.image}
-          transition={1000}
-        />
-        <ThemedButton title="hoiiiiiiiiii" style={styles.button} variant="secondary" onPress={() => router.push("/")} />
-      </ThemedView>
-
+    <ScreenView>
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <View style={styles.pinkBar} />
+        <ContainerView>
+          <Image
+            source={require("@/assets/images/logo.png")}
+            // placeholder={{ blurhash }}
+            contentFit="scale-down"
+            style={styles.image}
+            transition={1000}
+          />
+          <ThemedButton
+            title="hoiiiiiiiiii"
+            variant="secondary"
+            onPress={() => router.push("/select_frame")}
+          />
+        </ContainerView>
+      </View>
+    </ScreenView>
   );
-
 }
 
 const styles = StyleSheet.create({
@@ -34,10 +39,21 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  pinkBar: {
+    width: 24,
+    position: "absolute",
+    left: 40,
+    top: 0,
+    bottom: 0,
+    height: "100%",
+    backgroundColor: "#FFCEEE",
+    zIndex: 100,
+  },
   image: {
-    flex: 1,
-    width: '100%',
-    backgroundColor: '#0553'
+    width: "50%",
+    height: "50%",
+    minWidth: 250,
+    maxHeight: 250,
   },
   button: {
     backgroundColor: "#FFFFFF",
